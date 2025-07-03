9e1e32d2f25b16911cd6a3c1c394e667e687353b
Gaurav <sachdeva.gaurav2911@gmail.com>
2025-07-04 00:32:14 +0530

    Added 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return <div className="not-found">
+    <h1>404 - Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+    <Link href="/">Go back home</Link>
+  </div>
+}
